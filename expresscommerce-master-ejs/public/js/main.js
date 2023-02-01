25b91bb295201d60968c82f2fcae151066ac2a22
kumardhananjay123 <123064234+kumardhananjay123@users.noreply.github.com>
2023-02-01 14:37:03 +0530

    #left navigation list updated
    left navigation list updated

--- expresscommerce-master-ejs/public/js/main.js
+++ expresscommerce-master-ejs/public/js/main.js
@@ -8,4 +8,19 @@ $(document).ready(function () {
 
     $('#example').DataTable();
 
+    $('tr.submenu').click(function(evt){
+        var currentID = this.id || "No ID!";
+        if($('.d_'+currentID).hasClass("d-none")){
+            $(this).addClass('table-primary');
+            $('.d_'+currentID).removeClass('d-none');
+            $('.bi_'+currentID).removeClass('bi-plus-circle').addClass('bi-dash-circle');
+        }
+        else {
+            $('.d_'+currentID).addClass('d-none');
+            $('.bi_'+currentID).removeClass('bi-dash-circle').addClass('bi-plus-circle');
+            $(this).removeClass('table-primary');
+        }
+        
+      })
+
 });
