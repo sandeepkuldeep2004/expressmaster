e8187d5c8bd51402186b5bd38077a74ef26cc3c2
kumardhananjay123 <123064234+kumardhananjay123@users.noreply.github.com>
2023-02-02 15:48:38 +0530

    #Module assignment
    Module assignment

--- expresscommerce-master-ejs/public/js/main.js
+++ expresscommerce-master-ejs/public/js/main.js
@@ -20,7 +20,38 @@ $(document).ready(function () {
             $('.bi_'+currentID).removeClass('bi-dash-circle').addClass('bi-plus-circle');
             $(this).removeClass('table-primary');
         }
-        
-      })
+    })
+
+    // the selector will match all input controls of type :checkbox
+    // and attach a click event handler
+    var elems = [];
+    $("input.form-check-input:checkbox").on('click', function() {
+    // in the handler, 'this' refers to the box clicked on
+    var $box = $(this);
+    if ($box.is(":checked")) {
+    // the name of the box is retrieved using the .attr() method
+    // as it is assumed and expected to be immutable
+    var group = "input:checkbox[name='" + $box.attr("name") + "']";
+    
+    var $modulename = "{"+$box.attr("name")+":"+$box.attr("value")+"}";
+    console.log($modulename);
+
+    
+    elems.push($modulename);
+    $('#modulelistarray').val(JSON.stringify(elems)); //store array
+    
+    var value = $('#modulelistarray').val(); //retrieve array
+    value = JSON.parse(value);
+    console.log($("#modulelistarray").val());
+    // the checked state of the group/box on the other hand will change
+    // and the current value is retrieved using .prop() method
+    $(group).prop("checked", false);
+    $box.prop("checked", true);
+    } else {
+    var $modulename = "{"+$box.attr("name")+":"+$box.attr("value")+"}";
+    $box.prop("checked", false);
+    console.log($modulename);
+    }
+    });
 
 });
