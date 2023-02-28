947466c867506938cfe1a4d5632a9111d65a33d7
rakekumar1 <123063891+rakekumar1@users.noreply.github.com>
2023-02-28 16:27:00 +0530

    file upload example
    file upload example

--- expresscommerce-master-ejs/routes/backoffice/ajax.js
+++ expresscommerce-master-ejs/routes/backoffice/ajax.js
@@ -4,6 +4,14 @@ const { body, validationResult } = require("express-validator");
 const { ensureAuth } = require("../../middleware/auth");
 const UserGroupModel = require("../../models/UserGroup");
 const {getUserGroupsBybaseSiteService} = require("../../services/UserGroup.js");
+const {upload} = require("../../services/commons.js");
+
+var csrf = require('csurf')
+
+//var csrfProtection = csrf({ cookie: true })
+
+
+
 
 router.post("/getselectbox", ensureAuth, async (req, res) => {
   console.log("inasdfasdf"+req.body.basesitereg);
@@ -13,5 +21,38 @@ console.log("inasdfasdf"+userGroupBaseSiteList);
 res.send(userGroupBaseSiteList);
 });
 
+
+router.post("/uploadFile",async (req, res)=> {
+
+  console.log(req.body.mypic);
+        
+  // Error MiddleWare for multer file upload, so if any
+  // error occurs, the image would not be uploaded!
+  console.log("innnnnnnnnnn");
+
+  upload(req,res,function(err) {
+  
+    if(err) {
+
+        // ERROR occurred (here it can be occurred due
+        // to uploading image of size greater than
+        // 1MB or uploading different file type)
+       // res.send(err)
+      
+    }
+    else {
+
+        // SUCCESS, image successfully uploaded
+       // res.send("Success, Image uploaded!")
+    }
+  })
+
+  res.render("datasync/view",{
+    csrfToken: req.csrfToken(),
+  });
+ 
+
+  
+})
 
 module.exports = router;
--- expresscommerce-master-ejs/services/commons.js
+++ expresscommerce-master-ejs/services/commons.js
@@ -2,6 +2,10 @@ const { ensureAuth } = require('../middleware/auth')
 const CatalogModel = require('../models/Catalog')
 const CategoryModel = require('../models/Category')
 const CountryModel = require('../models/Country')
+const multer = require("multer")
+const path = require("path")
+
+
 
 // @desc    fetch all active Catalogs
 //@param {active}
@@ -22,6 +26,41 @@ const getCountryList = async function(param){
   return  CountryModel.find({}).sort({ name: 'asc' }).lean();
 }
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+
+      // Uploads is the Upload_folder_name
+      cb(null, "data/uploadImpex")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname +".json")
+  }
+})
+
+var upload = multer({ 
+  storage: storage,
+  limits: { fileSize: 1 * 1000 * 1000 },
+  fileFilter: function (req, file, cb){
+  
+      // Set the filetypes, it is optional
+      var filetypes = /json/;
+      var mimetype = filetypes.test(file.mimetype);
+      console.log(file);
+
+      var extname = filetypes.test(path.extname(
+                  file.originalname).toLowerCase());
+      
+      if (mimetype && extname) {
+          return cb(null, true);
+      }
+    
+      cb("Error: File upload only supports the "
+              + "following filetypes - " + filetypes);
+    } 
+
+// essentialData is the name of file attribute
+}).single("essentialData");   
+
 module.exports={
-    getCatalogList, getCategoryList,getCountryList
+    getCatalogList, getCategoryList,getCountryList,upload
 }
